94b8790a8a9bb0c6059da2f06771eda1419af417
Student <Student@1912018-C18081.lanecc.edu>
2018-11-19 13:32:35 -0800

    testing productprop

---
+++ FrameworkExampleEventSQL/EventTestClasses/ProductPropsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EventTestClasses
+{
+    [TestFixture]
+    public class ProductPropsTest
+    {
+        [Test]
+        //test the methods we wrote
+        public void TestClone()
+        {
+
+        }
+
+        public void TestGetState()
+        {
+
+        }
+
+        public void TestSetState()
+        {
+
+        }
+    }
+}
